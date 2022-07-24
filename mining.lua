70bc99e8ac75fcfc85d219fac3b8f62d04564121
ArcitcPhoenix <rhysman2016@gmail.com>
2022-07-24 17:53:49 -0500

    Update mining.lua

--- mining.lua
+++ mining.lua
@@ -2,6 +2,7 @@ local isOre
 local running = true
 
 
+
 local function check()
 
        turtle.inspect()
@@ -15,14 +16,33 @@ local function check()
        turtle.inspectDown()
 end
 
+local function mine()
+    if(isOre) then
+        turtle.dig
+        turtle.forward
+        check()
+    end
+end
+
+
     
 
 while running do
 
     local blockBool, blockInfo = turtle.inspect()
+    local upBlockBool, upBlockInfo = turtle.inspectUp()
+    local downBlockBool, downBlockInfo = turtle.inspectDown()
     isOre = blockBool and string.find(blockInfo.name, "ore")
-    
+    upIsOre = upBlockBool and string.find(upBlockInfo.name, "ore")
+    downIsOre = upBlockBool and string.find(downBlockInfo.name, "ore")
+
     if isOre then
         print("isOre")
+    end
+    if upIsOre then
+        print("upIsOre")
+    end
+    if downIsOre then
+        print("downIsOre")
     end
     end
