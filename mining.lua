0a6d862e42dcf323c917271c3ed723e8dd79702a
ArcitcPhoenix <rhysman2016@gmail.com>
2022-07-23 21:02:01 -0500

    Update mining.lua

--- mining.lua
+++ mining.lua
@@ -2,34 +2,24 @@ local blockBool, blockInfo = turtle.inspect()
 local isOre
 local running = true
 
+local function check()
+
+       turtle.inspect()
+       turtle.turnLeft()
+       turtle.inspect()
+       turtle.turnRight()
+       turtle.turnRight()
+       turtle.inspect()
+       turtle.turnLeft()
+       turtle.inspectUp()
+       turtle.inspectDown()
+end
 
     
 
 while running do
     isOre = blockBool and string.find(blockInfo.name, "ore")
-    turtle.inspect()
-    if (isOre == true) then
-        print(blockInfo)
-    end
-    turtle.turnLeft()
-    blockBool()
-    if (isOre == true) then
-        print(blockInfo)
-        turtle.dig()
-    end
-    turtle.turnRight()
-    turtle.turnRight()
-    turtle.inspect()
-    if (isOre == true) then
-        print(blockInfo)
-    end
-    turtle.turnLeft()
-    turtle.inspectUp()
-    if (isOre == true) then
-        print(blockInfo)
-    end
-    turtle.inspectDown()
-    if (isOre == true) then
-        print(blockInfo)
+    if isOre == true then
+        print("isOre")
     end
     end
